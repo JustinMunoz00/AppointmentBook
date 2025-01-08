315cefeef97bae8f4de99fd32a5e05e2d2cb89d2
JustinMunoz00 <justinm880@nycstudents.net>
2025-01-08 16:50:59 +0000

    Starting other 2 methods and completed test cases for one method

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -2,14 +2,17 @@ public class AppointmentBook {
     
     private boolean[][] schedule;
 
-    
+    public AppointmentBook(boolean[][] schedule){
+        this.schedule = schedule;
+    }
     /**
      * Returns true if minute in period is available for an appointment and returns
      * false otherwise
      * Preconditions: 1 <= period <= 8; 0 <= minute <= 59
      */
     private boolean isMinuteFree(int period, int minute) {
-        /* implementation not shown */ }
+        return schedule[period-1][minute];
+    }
 
     /**
      * Marks the block of minutes that starts at startMinute in period and
@@ -29,7 +32,16 @@ public class AppointmentBook {
      * Preconditions: 1 <= period <= 8; 1 <= duration <= 60
      */
     public int findFreeBlock(int period, int duration) {
-        /* to be implemented in part (a) */ }
+        int count = 0;
+        for(int i = 0; i < 60; i++){
+            if (isMinuteFree(period, i)){
+                count++;
+                if (count == duration) return i - count + 1;
+            }
+            else count = 0;
+        }
+        return -1;
+    }
 
     /**
      * Searches periods from startPeriod to endPeriod, inclusive, for a block
@@ -40,7 +52,8 @@ public class AppointmentBook {
      * Preconditions: 1 <= startPeriod <= endPeriod <= 8; 1 <= duration <= 60
      */
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
-     /* to be implemented in part (b) */ }
+        return false;
+    }
 
     // There may be instance variables, constructors, and methods that are not shown.
 
--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
 public class Main {
-    
+    public static void main (String[] args){
+        boolean[][] schedule1 = new boolean[8][60];
+        for (int i = 10; i < 15; i++) schedule1[1][i] = true;
+        for (int i = 30; i < 45; i++) schedule1[1][i] = true;
+        for (int i = 50; i < 60; i++) schedule1[1][i] = true;
+        AppointmentBook a = new AppointmentBook(schedule1);
+        
+        // findFreeBlock test cases
+        System.out.println(a.findFreeBlock(2, 15));
+        System.out.println(a.findFreeBlock(2, 9));
+        System.out.println(a.findFreeBlock(2, 20));
+
+
+    }
 }
